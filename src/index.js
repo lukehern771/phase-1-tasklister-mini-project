5d2c526130201dd9aeb0966cb74a7746f2586d8b
Luke <hluke9242@gmail.con>
2023-07-21 14:50:26 -0600

    complete mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const newTaskForm = document.querySelector('form')
+  newTaskForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const newToDo = e.target['new-task-description'].value
+    buildToDo(newToDo); newTaskForm.reset()
+  })
 });
+
+function buildToDo(newToDo){
+  let taskLi = document.createElement('Li');
+  let deleteButton = document.createElement('button')
+  deleteButton.textContent = 'x'
+  taskLi.textContent = `${newToDo}`
+  taskLi.appendChild(deleteButton);
+  document.querySelector('#tasks').appendChild(taskLi);
+  deleteButton.addEventListener('click', handleDelete)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove();
+}
